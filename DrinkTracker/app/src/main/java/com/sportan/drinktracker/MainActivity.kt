356af5c6e7b6df62947b553849a7865d09f225bc
samportan <samijtucker@gmail.com>
2023-05-23 09:14:14 -0600

    first commit

---
+++ DrinkTracker/app/src/main/java/com/sportan/drinktracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sportan.drinktracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
